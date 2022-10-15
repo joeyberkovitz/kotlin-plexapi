c75e921e687fe2053304edc1d121cdcf637b85d0
Joey Berkovitz <joey@berkovitz.us>
2022-10-15 18:21:23 -0400

    github build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-val ktor_version: String by project
+val ktorVersion: String by project
 
 plugins {
 	val kotlinVersion = "1.7.20"
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "us.berkovitz"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
 	mavenCentral()
@@ -30,17 +30,35 @@ kotlin {
 			dependencies {
 				implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
 				implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-				implementation("io.ktor:ktor-client-core:$ktor_version")
-				implementation("io.ktor:ktor-client-cio:$ktor_version")
-				implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-				implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-				implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktor_version")
+				implementation("io.ktor:ktor-client-core:$ktorVersion")
+				implementation("io.ktor:ktor-client-cio:$ktorVersion")
+				implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+				implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+				implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktorVersion")
 			}
 		}
 		val jvmTest by getting {
 			dependencies {
 				implementation(kotlin("test"))
-				implementation("io.ktor:ktor-client-mock:$ktor_version")
+				implementation("io.ktor:ktor-client-mock:$ktorVersion")
+			}
+		}
+	}
+
+	publishing {
+		repositories {
+			maven {
+				name = "GitHubPackages"
+				url = uri("https://maven.pkg.github.com/joeyberkovitz/kotlin-plexapi")
+				credentials {
+					username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+					password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+				}
+			}
+		}
+		publications {
+			register<MavenPublication>("gpr") {
+				from(components["java"])
 			}
 		}
 	}
