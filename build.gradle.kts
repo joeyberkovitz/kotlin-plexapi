8b3a100ce9b3626544c9aad5d1ff57a1105efb8f
Joey Berkovitz <joey@berkovitz.us>
2023-07-22 17:44:05 -0400

    support integer booleans

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 group = "us.berkovitz"
-version = "0.1.12"
+version = "0.1.13"
 
 repositories {
 	mavenCentral()
--- src/jvmMain/kotlin/us/berkovitz/plexapi/myplex/Common.kt
+++ src/jvmMain/kotlin/us/berkovitz/plexapi/myplex/Common.kt
@@ -3,10 +3,30 @@ package us.berkovitz.plexapi.myplex
 import io.ktor.client.call.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlValue
+
+@Serializable
+data class PlexBool(val value: Boolean) {
+    @Serializer(forClass=PlexBool::class)
+    companion object: KSerializer<PlexBool> {
+        override fun deserialize(decoder: Decoder): PlexBool {
+            val strVal = decoder.decodeString()
+            val intVal = strVal.toIntOrNull()
+            return PlexBool(intVal == 1)
+        }
+
+        override fun serialize(encoder: Encoder, value: PlexBool) {
+            encoder.encodeString(if(value.value) "1" else "0")
+        }
+    }
+}
 
 @Serializable
 @SerialName("error")
